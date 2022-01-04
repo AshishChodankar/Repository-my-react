f89158362fafc29f3c89507813b56d612fe06c9c
AshishChodankar <acchodankar@gmail.com>
2022-01-04 23:36:48 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 export default function App() {
-  let list = [1, 1, 1, 1, 1];
+  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   return (
     <div className="container-fluid">
-      <div className="row bg-dark">
-        <div className="col h3 text-light">React</div>
+      <div className="row bg-danger text-light p-3 sticky-top">
+        <div className="col h3 ">React</div>
       </div>
+
+      {list.map((item) => (
+        <div className="row">
+          <div className="col-12 bg-secondary text-light p-3 mt-2 ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+            eos fugiat recusandae accusamus? Illum odit ipsa rem labore
+            officiis, fugiat dignissimos praesentium doloribus delectus
+            similique soluta deserunt! Debitis, expedita facere!
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
