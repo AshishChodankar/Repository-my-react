1a7025f5448b4ce72c837895af6efb5f34be7f1f
AshishChodankar <acchodankar@gmail.com>
2022-01-04 19:27:46 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import "./App.css";
 export default function App() {
   return (
     <div>
-      <div classname="bg-primary text-light p-4">Hello World</div>
+      <div className="beautify">Hello World</div>
     </div>
   );
 }
