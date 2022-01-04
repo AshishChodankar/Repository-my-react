a57b45a25cb61077df303b55e188131b27d28acd
AshishChodankar <acchodankar@gmail.com>
2022-01-04 21:56:21 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 export default function App() {
+  let list = [1, 1, 1, 1, 1];
   return (
     <div className="container-fluid">
-      <div className="row bg-danger text-light p-3">
-        <div className="col h3 ">React</div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 bg-secondary text-light p-3">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eos
-          fugiat recusandae accusamus? Illum odit ipsa rem labore officiis,
-          fugiat dignissimos praesentium doloribus delectus similique soluta
-          deserunt! Debitis, expedita facere!
-        </div>
+      <div className="row bg-dark">
+        <div className="col h3 text-light">React</div>
       </div>
     </div>
   );
 }
-
-/*
-export default function App() {
-  return (
-    <div>
-      <div className="Container-fluid">
-        <div className="row bg-dark justify-content-center">
-          <div className="col-md-3 justify-content-center">React</div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col"></div>
-      </div>
-    </div>
-  );
-}
-*/
