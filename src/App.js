a88e345413756cc41484643907d8f57656caf077
AshishChodankar <acchodankar@gmail.com>
2022-01-05 11:01:05 +0530

    b16

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import HelloWorld1 from "./components/HelloWorld1.js";
-import HelloUniverse from "./components/HelloUniverse.js";
-export default function test() {
+export default function App() {
   return (
-    <>
-      <HelloWorld1 />
-      <HelloUniverse />
-    </>
+    <div className="container-fluid">
+      <header />
+      <h1>Hello</h1>
+      <MyCard />
+    </div>
   );
 }
+
+function header() {
+  return (
+    <div className="row bg-dark text-light">
+      <div className="col">MyReact</div>;
+    </div>
+  );
+}
+
+function MyCard() {}
